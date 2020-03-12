cc00214401933ecf89fc2c0744c932a8fb9c9d1a
joshua-nanakojo-tetteh <54946824+joshua-nanakojo-tetteh@users.noreply.github.com>
2020-03-12 22:44:48 +0000

    Add files via upload

---
+++ tips.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$tips = [
+    'Don’t drink sugar calories',
+    'Avoid processed junk food (eat real food instead)',
+    'Don’t fear coffee',
+    'Eat fatty fish',
+    'Get enough sleep',
+    'Take care of your gut health with probiotics and fiber',
+    'Drink some water, especially before meals',
+    'Don’t overcook or burn your meat',
+    'Avoid bright lights before sleep',
+    'Take vitamin D3 if you don’t get much sun exposure',
+    'Eat vegetables and fruits',
+    'Make sure to eat enough protein',
+    'Don’t smoke or do drugs, and only drink in moderation',
+    'Use extra virgin olive oil',
+    'Minimize your sugar intake',
+    'Don’t eat a lot of refined carbs',
+    'Don’t fear saturated fat',
+    'Lift heavy things',
+    'Avoid artificial trans fats',
+    'Use plenty of herbs and spices',
+    'Take care of your relationships',
+    'Track your food intake every now and then',
+    'If you have excess belly fat, get rid of it',
+    'Don’t go on a diet',
+];
